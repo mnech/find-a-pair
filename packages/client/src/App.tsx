e59f9bf58a38fc45672dfe0bdaaca7747a963117
Maria Necheporenko <mari.necheporenko@gmail.com>
2023-02-06 12:18:17 +0200

    Исправление ошибки bootstrap

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss'
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from './components/router/AppRouter';
