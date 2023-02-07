ac581c82189c9f479c0f9b79626696739f2c8ff3
Maria Necheporenko <mari.necheporenko@gmail.com>
2023-02-07 20:14:43 +0200

    Правки по ревью

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,17 @@
 import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from './components/router/AppRouter';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Page404 from "./pages/Page404/Page404";
+import Profile from "./pages/Profile/Profile";
+import Game from "./pages/Game/Game";
+import Leaderboard from "./pages/Leaderboard/Leaderboard";
+import Forum from "./pages/Forum/Forum";
+
+//пути к папкам нужно поменять, когда будут готовы страницы
+import Signin from "./pages/Profile/Profile";
+import Signup from "./pages/Profile/Profile";
 
 function App() {
   useEffect(() => {
@@ -18,7 +27,52 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <AppRouter />
+      <Routes>
+        <Route path="/">
+          <Route 
+            index
+            element={
+                <Profile />
+            }
+          />
+
+          <Route 
+            path="sigin"
+            element={
+                <Signin />
+            }
+          />
+
+          <Route 
+            path="signup"
+            element={
+                <Signup />
+            }
+          />
+
+          <Route 
+            path="leaderboard"
+            element={
+                <Leaderboard />
+            }
+          />
+
+          <Route 
+            path="forum"
+            element={
+                <Forum />
+            }
+          />
+
+          <Route 
+            path="game"
+            element={
+                <Game />
+            }
+          />    
+        </Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
       </div>
     </BrowserRouter>
   )
