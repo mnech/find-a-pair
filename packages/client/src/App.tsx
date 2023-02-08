f0b23928ea8e94c6ac62d1ac162e76820dff1b5c
Maria Necheporenko <mari.necheporenko@gmail.com>
2023-02-08 21:02:38 +0200

    Исправление ошибок

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,8 @@
 import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-
-import Page404 from "./pages/Page404/Page404";
-import Profile from "./pages/Profile/Profile";
-import Game from "./pages/Game/Game";
-import Leaderboard from "./pages/Leaderboard/Leaderboard";
-import Forum from "./pages/Forum/Forum";
-
-//пути к папкам нужно поменять, когда будут готовы страницы
-import Signin from "./pages/Profile/Profile";
-import Signup from "./pages/Profile/Profile";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from './components/router/AppRouter';
 
 function App() {
   useEffect(() => {
@@ -27,52 +18,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <Routes>
-        <Route path="/">
-          <Route 
-            index
-            element={
-                <Profile />
-            }
-          />
-
-          <Route 
-            path="sigin"
-            element={
-                <Signin />
-            }
-          />
-
-          <Route 
-            path="signup"
-            element={
-                <Signup />
-            }
-          />
-
-          <Route 
-            path="leaderboard"
-            element={
-                <Leaderboard />
-            }
-          />
-
-          <Route 
-            path="forum"
-            element={
-                <Forum />
-            }
-          />
-
-          <Route 
-            path="game"
-            element={
-                <Game />
-            }
-          />    
-        </Route>
-        <Route path="*" element={<Page404 />} />
-      </Routes>
+        <AppRouter />
       </div>
     </BrowserRouter>
   )
