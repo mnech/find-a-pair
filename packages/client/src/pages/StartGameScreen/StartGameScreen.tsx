6eb9d058358e7c9186ca9bc4856f9b1b444dd6db
Maria Necheporenko <mari.necheporenko@gmail.com>
2023-02-12 10:14:47 +0200

    Замена кнопки старта на ссылку

--- packages/client/src/pages/StartGameScreen/StartGameScreen.tsx
+++ packages/client/src/pages/StartGameScreen/StartGameScreen.tsx
@@ -1,22 +1,15 @@
-import { Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import "./StartGameScreen.scss";
 
 export const StartGameScreen = () => {
-  const navigate = useNavigate();
-
-  const startGame = () => {
-    navigate("/game");
-  }
-
   return (
     <div className="start">
       <div className="start__container">
         <p className="start__title">Кликай по карточкам, запоминай, что под ними изображено, и находи пары одинаковых картинок</p>
-          <Button onClick={startGame}>
+          <Link to="/game" className='start__link'>
             Старт
-          </Button>
+          </Link>
         </div>
     </div>
   )
