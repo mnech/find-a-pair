8c2074c345a9d4f93dbcb8b46af32977be47a5f3
talanov0404 <talanov04041996@yandex.ru>
2023-02-13 00:35:57 +0300

    Правки английского))

--- packages/client/src/pages/Profile/components/EditDataForm/EditDataForm.tsx
+++ packages/client/src/pages/Profile/components/EditDataForm/EditDataForm.tsx
@@ -1,120 +1,125 @@
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
-import { UserData } from '../../../../models/User';
-import UsersController from '../../../../controllers/UsersController';
-import { regexpTest } from '../../../../utils/validate';
-import { FormGroup } from '../../../../components/FormGroup';
-import './EditDataForm.scss';
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
+import { UserData } from '../../../../models/User'
+import UsersController from '../../../../controllers/UsersController'
+import { regexpTest } from '../../../../utils/validate'
+import { FormGroup } from '../../../../components/FormGroup'
+import './EditDataForm.scss'
 
 export interface EditDataFormProps {
-  data: UserData;
-  setIsEditData: (state: boolean) => void,
+  data: UserData
+  setIsEditData: (state: boolean) => void
 }
 
 type TFormGroups = {
   [key in keyof UserData]: {
-    label: string;
-    placeholder: string;
-    errorText: string;
-    required: boolean;
+    label: string
+    placeholder: string
+    errorText: string
+    required: boolean
     type?: string
-  };
-};
+  }
+}
 
 const FormGroups: TFormGroups = {
   first_name: {
-    label: 'First name',
-    placeholder: 'Enter first name',
-    errorText: 'Please provide a valid first name',
+    label: 'Имя',
+    placeholder: 'Введите имя',
+    errorText: 'Введите верное имя',
     required: true,
   },
   second_name: {
-    label: 'Second name',
-    placeholder: 'Enter second name',
+    label: 'Фамилия',
+    placeholder: 'Введите фамилию',
     required: true,
-    errorText: 'Please provide a valid second name',
+    errorText: 'Введите верную фамилию',
   },
   display_name: {
-    label: 'Display name',
-    placeholder: 'Enter display name',
+    label: 'Псевдоним',
+    placeholder: 'Введите псевдоним',
     required: true,
-    errorText: 'Please provide a valid display name',
+    errorText: 'Введите верный псевдоним',
   },
   login: {
-    label: 'Login',
-    placeholder: 'Enter login',
+    label: 'Логин',
+    placeholder: 'Введите логин',
     required: true,
-    errorText: 'Please provide a valid login',
+    errorText: 'Введите верный логин',
   },
   email: {
     label: 'Email',
-    placeholder: 'Enter email',
+    placeholder: 'Введите email',
     required: true,
-    errorText: 'Please provide a valid email',
+    errorText: 'Введите верный email',
   },
   phone: {
-    label: 'Phone',
-    placeholder: 'Enter second name',
+    label: 'Номер телефона',
+    placeholder: 'Введите номер телефона',
     required: true,
-    errorText: 'Please provide a valid phone',
+    errorText: 'Введите верный номер телефона',
     type: 'tel',
   },
-} as const;
+} as const
 
 const getValidateInit = (data: UserData) => {
   return Object.keys(FormGroups).reduce((acc, key) => {
-    acc[key as keyof UserData] = !!data[key as keyof UserData];
-    return acc;
-  }, {} as Record<keyof UserData, boolean>);
-};
+    acc[key as keyof UserData] = !!data[key as keyof UserData]
+    return acc
+  }, {} as Record<keyof UserData, boolean>)
+}
 
 const EditDataForm = ({ data, setIsEditData }: EditDataFormProps) => {
-  const [validated, setValidated] = useState(() => getValidateInit(data));
+  const [validated, setValidated] = useState(() => getValidateInit(data))
 
-  const handleSubmit = async (event: React.SyntheticEvent<HTMLFormElement, Event>) => {
-    event.stopPropagation();
-    event.preventDefault();
-    const form = event.currentTarget;
-    if (!Object.values(validated).every(item => item)) return;
+  const handleSubmit = async (
+    event: React.SyntheticEvent<HTMLFormElement, Event>
+  ) => {
+    event.stopPropagation()
+    event.preventDefault()
+    const form = event.currentTarget
+    if (!Object.values(validated).every(item => item)) return
 
-    const data = Object.fromEntries(new FormData(form));
-    await UsersController.updateProfile(data as unknown as UserData);
+    const data = Object.fromEntries(new FormData(form))
+    await UsersController.updateProfile(data as unknown as UserData)
 
-    setIsEditData(false);
-  };
+    setIsEditData(false)
+  }
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = event.currentTarget;
-    setValidated(
-      {
-        ...validated,
-        [name]: regexpTest(name as keyof UserData, value),
-      });
-  };
+    const { value, name } = event.currentTarget
+    setValidated({
+      ...validated,
+      [name]: regexpTest(name as keyof UserData, value),
+    })
+  }
 
-  return <Form className="form-edit-data" noValidate onSubmit={handleSubmit}>
-    {Object.entries(FormGroups).map(([key, value]) => {
-      const { label, placeholder, errorText, required, type } = value;
-      const name = key as keyof UserData;
+  return (
+    <Form className="form-edit-data" noValidate onSubmit={handleSubmit}>
+      {Object.entries(FormGroups).map(([key, value]) => {
+        const { label, placeholder, errorText, required, type } = value
+        const name = key as keyof UserData
 
-      return (<FormGroup
-        key={name}
-        type={type}
-        label={label}
-        defaultValue={data[name]}
-        name={name}
-        placeholder={placeholder}
-        required={required}
-        errorText={errorText}
-        isInvalid={!validated[name]}
-        onChange={onChange}
-      />);
-    })}
-    <Button variant="primary" type="submit" className="button">
-      Submit
-    </Button>
-  </Form>;
-};
+        return (
+          <FormGroup
+            key={name}
+            type={type}
+            label={label}
+            defaultValue={data[name]}
+            name={name}
+            placeholder={placeholder}
+            required={required}
+            errorText={errorText}
+            isInvalid={!validated[name]}
+            onChange={onChange}
+          />
+        )
+      })}
+      <Button variant="primary" type="submit" className="button">
+        Submit
+      </Button>
+    </Form>
+  )
+}
 
-export default EditDataForm;
+export default EditDataForm
--- packages/client/src/pages/Profile/components/EditPasswordForm/EditPasswordForm.tsx
+++ packages/client/src/pages/Profile/components/EditPasswordForm/EditPasswordForm.tsx
@@ -1,131 +1,138 @@
-import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import { Button } from 'react-bootstrap';
-import UsersController from '../../../../controllers/UsersController';
-import { Password } from '../../../../models/User';
-import { regexpTest } from '../../../../utils/validate';
-import { FormGroup } from '../../../../components/FormGroup';
-import './EditPasswordForm.scss';
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import { Button } from 'react-bootstrap'
+import UsersController from '../../../../controllers/UsersController'
+import { Password } from '../../../../models/User'
+import { regexpTest } from '../../../../utils/validate'
+import { FormGroup } from '../../../../components/FormGroup'
+import './EditPasswordForm.scss'
 
 export interface EditPasswordFormProps {
-  setIsEditPassword: (state: boolean) => void,
+  setIsEditPassword: (state: boolean) => void
 }
 
 type TPasswordFields = {
-  oldPassword: string;
-  newPassword: string;
+  oldPassword: string
+  newPassword: string
   repeatNewPassword: string
 }
 
 type TFormGroups = {
   [key in keyof TPasswordFields]: {
-    label: string;
-    placeholder: string;
-    errorText: string;
-    required: boolean;
+    label: string
+    placeholder: string
+    errorText: string
+    required: boolean
     type?: string
-  };
-};
+  }
+}
 
 const FormGroups: TFormGroups = {
   oldPassword: {
-    label: 'Old password',
-    placeholder: 'Enter old password',
-    errorText: 'Please provide a valid old password',
+    label: 'Старый пароль',
+    placeholder: 'Введите старый пароль',
+    errorText: 'Введите верный старый пароль',
     required: true,
     type: 'password',
   },
   newPassword: {
-    label: 'New password',
-    placeholder: 'Enter new password',
+    label: 'Новый пароль',
+    placeholder: 'Введите новый пароль',
     required: true,
-    errorText: 'Please provide a valid new password',
+    errorText: 'Введите верный новый пароль',
     type: 'password',
   },
   repeatNewPassword: {
-    label: 'Repeat new password',
-    placeholder: 'Repeat new password',
+    label: 'Повторите новый пароль',
+    placeholder: 'Введите повторно новый пароль',
     required: true,
-    errorText: 'Password mismatch',
+    errorText: 'Пароли не совпадают',
     type: 'password',
   },
-} as const;
+} as const
 
 const getValidateInit = () => {
   return Object.keys(FormGroups).reduce((acc, key) => {
-    acc[key as keyof TFormGroups] = false;
-    return acc;
-  }, {} as Record<keyof TFormGroups, boolean>);
-};
+    acc[key as keyof TFormGroups] = false
+    return acc
+  }, {} as Record<keyof TFormGroups, boolean>)
+}
 
 const getValuesInit = () => {
   return Object.keys(FormGroups).reduce((acc, key) => {
-    acc[key as keyof TFormGroups] = null;
-    return acc;
-  }, {} as Record<keyof TFormGroups, string | null>);
-};
+    acc[key as keyof TFormGroups] = null
+    return acc
+  }, {} as Record<keyof TFormGroups, string | null>)
+}
 
 const EditPasswordForm = ({ setIsEditPassword }: EditPasswordFormProps) => {
-  const [validated, setValidated] = useState(() => getValidateInit());
-  const [values, setValues] = useState(() => getValuesInit());
+  const [validated, setValidated] = useState(() => getValidateInit())
+  const [values, setValues] = useState(() => getValuesInit())
 
   const handleSubmit = async (event: React.FormEvent) => {
-    event.stopPropagation();
-    event.preventDefault();
-    const form = event.currentTarget as HTMLFormElement;
-    if (!Object.values(validated).every(item => item)) return;
+    event.stopPropagation()
+    event.preventDefault()
+    const form = event.currentTarget as HTMLFormElement
+    if (!Object.values(validated).every(item => item)) return
 
-    const data = Object.fromEntries(new FormData(form));
-    await UsersController.updatePassword(data as unknown as Password);
+    const data = Object.fromEntries(new FormData(form))
+    await UsersController.updatePassword(data as unknown as Password)
 
-    setIsEditPassword(false);
-  };
+    setIsEditPassword(false)
+  }
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = event.currentTarget;
-    setValidated(
-      {
-        ...validated,
-        [name]: regexpTest('password', value),
-      });
-    setValues({ ...values, [name]: value });
-  };
+    const { value, name } = event.currentTarget
+    setValidated({
+      ...validated,
+      [name]: regexpTest('password', value),
+    })
+    setValues({ ...values, [name]: value })
+  }
 
-  const onChangeRepeatPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = event.currentTarget;
-    setValues({ ...values, [name]: value });
-    setValidated(
-      {
-        ...validated,
-        [name]: values.newPassword === values.repeatNewPassword && !!values.newPassword,
-      });
-  };
+  const onChangeRepeatPassword = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { value, name } = event.currentTarget
+    setValues({ ...values, [name]: value })
+    setValidated({
+      ...validated,
+      [name]:
+        values.newPassword === values.repeatNewPassword && !!values.newPassword,
+    })
+  }
 
   const isValid = (name: keyof TFormGroups) => {
-    return Boolean(validated[name] && values[name]);
-  };
+    return Boolean(validated[name] && values[name])
+  }
 
-  return <Form className="form-edit-password" noValidate onSubmit={handleSubmit}>
-    {Object.entries(FormGroups).map(([key, value]) => {
-      const { label, placeholder, errorText, required, type } = value;
-      const name = key as keyof TFormGroups;
+  return (
+    <Form className="form-edit-password" noValidate onSubmit={handleSubmit}>
+      {Object.entries(FormGroups).map(([key, value]) => {
+        const { label, placeholder, errorText, required, type } = value
+        const name = key as keyof TFormGroups
 
-      return (<FormGroup
-        key={name}
-        type={type}
-        label={label}
-        name={name}
-        placeholder={placeholder}
-        required={required}
-        errorText={errorText}
-        isInvalid={!isValid(name)}
-        onChange={name !== 'repeatNewPassword' ? onChange : onChangeRepeatPassword}
-      />);
-    })}
-    <Button variant="primary" type="submit" className="button">
-      Submit
-    </Button>
-  </Form>;
-};
+        return (
+          <FormGroup
+            key={name}
+            type={type}
+            label={label}
+            name={name}
+            placeholder={placeholder}
+            required={required}
+            errorText={errorText}
+            isInvalid={!isValid(name)}
+            onChange={
+              name !== 'repeatNewPassword' ? onChange : onChangeRepeatPassword
+            }
+          />
+        )
+      })}
+      <Button variant="primary" type="submit" className="button">
+        Submit
+      </Button>
+    </Form>
+  )
+}
 
-export default EditPasswordForm;
+export default EditPasswordForm
