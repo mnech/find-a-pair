c59f4b779beba0f1cf06dad414f5e067dc82ac41
Sergei Sviridov <asgard998@yandex.ru>
2023-02-14 20:18:22 +0200

    0.6

--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -1,24 +1,19 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 import "./game.scss";
-import { GameExe } from './GameExe';
+import { GameExe } from "./GameExe";
 
-
-function Game(){
+function Game() {
   const ref = useRef(null);
 
   useEffect(() => {
-    const canvas = ref.current! as HTMLCanvasElement
+    const canvas = ref.current! as HTMLCanvasElement;
 
-    const ctx = canvas!.getContext('2d')
-  
-   // const ctx:React.MutableRefObject<null> = ref.current!.getContext('2d') as HTMLCanvasElement;
+    const ctx = canvas!.getContext("2d");
 
-    new GameExe(canvas, ctx)
+    new GameExe(canvas, ctx);
   }, []);
 
-  return (
-    <canvas ref={ref} width={500} height={500}/>
-  );
+  return <canvas ref={ref} width={500} height={500} />;
 }
 
 export default Game;
