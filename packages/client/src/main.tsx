f7a4795f1db8f12d7b8511ca82dd91fc63756733
19039680 <MECherdantseva@sberbank.ru>
2023-02-14 13:28:19 +0300

    222-2 Правки по замечаниям к пр

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App'
-import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
