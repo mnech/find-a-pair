4d3c048e3028e353029ffc646a0b78664000fa30
Sergei Sviridov <asgard998@yandex.ru>
2023-02-15 13:10:42 +0200

    0.1

--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -1,11 +1,19 @@
+import React, { useEffect, useRef } from "react";
 import "./game.scss";
+import { GameExe } from "./GameExe";
 
 function Game() {
-  return (
-    <div>
-      game
-    </div>
-  );
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const canvas = ref.current! as HTMLCanvasElement;
+
+    const ctx = canvas!.getContext("2d");
+
+    new GameExe(canvas, ctx);
+  }, []);
+
+  return <canvas ref={ref} width={500} height={500} />;
 }
 
 export default Game;
