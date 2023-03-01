c9aed28cf097ffdb4fa964911d6c06351f41538a
Maria Necheporenko <mari.necheporenko@gmail.com>
2023-03-01 22:04:24 +0200

    Исправление по ревью

--- packages/client/src/hooks/useAudio.ts
+++ packages/client/src/hooks/useAudio.ts
@@ -1,20 +1,28 @@
 import { useState, useEffect, useRef } from 'react';
 
+type AudioRef = {
+  current: null | HTMLAudioElement;
+};
+
 const useAudio = (url: string): [boolean, () => void] => {
-  const audioRef = useRef(new Audio(url));
+  const audioRef: AudioRef = useRef(null);
   const [playing, setPlaying] = useState(false);
+
+  if (audioRef.current === null) {
+    audioRef.current = new Audio(url);
+  }
 
   const toggle = () => setPlaying((playing) => !playing);
   const stopPlaying = () => setPlaying(false);
 
   useEffect(() => {
-    playing ? audioRef.current.play() : audioRef.current.pause();
+    playing ? audioRef.current?.play() : audioRef.current?.pause();
   }, [playing]);
 
   useEffect(() => {
-    audioRef.current.addEventListener('ended', stopPlaying);
+    audioRef.current?.addEventListener('ended', stopPlaying);
     return () => {
-      audioRef.current.removeEventListener('ended', stopPlaying);
+      audioRef.current?.removeEventListener('ended', stopPlaying);
     };
   }, []);
 
