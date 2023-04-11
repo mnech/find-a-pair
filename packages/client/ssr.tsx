5ae8b39bdc7f369a94e2fe5f238750eb4408560b
19039680 <MECherdantseva@sberbank.ru>
2023-04-11 19:34:34 +0300

    заработали роутинги

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,21 +1,21 @@
 import { renderToString } from 'react-dom/server';
-import App from './src/App';
 import { store } from './src/store';
 import { StaticRouter } from 'react-router-dom/server';
 import { Provider } from 'react-redux';
 import * as React from 'react';
+import AppRouter from './src/components/router/AppRouter';
 
 async function render(uri: string) {
   const initialState = store.getState();
   const renderResult = renderToString(
-    <Provider store={store}>
-      <StaticRouter location={uri}>
-        <App />
-      </StaticRouter>
-    </Provider>,
+    <React.StrictMode>
+      <Provider store={store}>
+        <StaticRouter location={uri}>
+          <AppRouter />
+        </StaticRouter>
+      </Provider>
+    </React.StrictMode>,
   );
-  console.log('renderResult', renderResult);
-  console.log('initialState', initialState);
   return [initialState, renderResult];
 }
 
