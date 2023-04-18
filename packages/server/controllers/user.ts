55ec6e59cbe38c041c28c570eaa0479fdc79b901
Maria <cherdantseva.m@yandex.ru>
2023-04-18 21:52:41 +0300

    Правки модели и контроллера пользователей

--- packages/server/controllers/user.ts
+++ packages/server/controllers/user.ts
@@ -7,8 +7,8 @@ export const checkUserExist: RequestHandler = async (req, res) => {
 
   const isUserExist = await User.count({
     where: {
-      id: {
-        [Op.eq]: id,
+      user_id: {
+        [Op.eq]: Number(id),
       },
     },
   });
@@ -16,6 +16,6 @@ export const checkUserExist: RequestHandler = async (req, res) => {
 };
 
 export const addUser: RequestHandler = async (req, res) => {
-  const addUser = await User.create(req.body);
+  const addUser = await User.create({ ...req.body, user_id: req.body.id });
   return res.json(addUser);
 };
